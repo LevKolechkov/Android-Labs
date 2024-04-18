15bd2f19ffef138ac7015607cf21e727031e8346
LevKolechkov <levkolechkov@gmail.com>
2024-04-19 01:50:22 +0700

    Добавлен ImageUri

--- Lab_2-5 Canvas/app/src/main/java/com/example/drawapp/MainActivity.kt
+++ Lab_2-5 Canvas/app/src/main/java/com/example/drawapp/MainActivity.kt
@@ -1,24 +1,44 @@
 package com.example.drawapp
 
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.activity.ComponentActivity
+import androidx.activity.compose.rememberLauncherForActivityResult
 import androidx.activity.compose.setContent
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.gestures.detectDragGestures
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateListOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.runtime.snapshots.SnapshotStateList
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Path
 import androidx.compose.ui.graphics.StrokeCap
+import androidx.compose.ui.graphics.asImageBitmap
 import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.input.pointer.pointerInput
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
 import com.example.drawapp.ui.theme.DrawAppTheme
 
 class MainActivity : ComponentActivity() {
@@ -33,7 +53,40 @@ class MainActivity : ComponentActivity() {
       }
       DrawAppTheme {
         Column {
-          DrawCanvas(pathData, pathList)
+
+          var imageUri by remember { mutableStateOf<Uri?>(null) }
+          val context = LocalContext.current
+          val bitmap = remember { mutableStateOf<Bitmap?>(null) }
+
+          val launcher =
+            rememberLauncherForActivityResult(contract = ActivityResultContracts.GetContent()) { uri: Uri? ->
+              imageUri = uri
+            }
+
+          Box(
+          ) {
+            imageUri?.let {
+              if (Build.VERSION.SDK_INT < 28){
+                bitmap.value = MediaStore.Images
+                  .Media.getBitmap(context.contentResolver, it)
+              } else {
+                val source = ImageDecoder.createSource(context.contentResolver, it)
+                bitmap.value = ImageDecoder.decodeBitmap(source)
+              }
+
+              bitmap.value?.let { btm ->
+                Image(
+                  bitmap = btm.asImageBitmap(),
+                  contentDescription = null,
+                  modifier = Modifier
+                    .size(400.dp)
+                    .padding(20.dp)
+                )
+              }
+            }
+            
+            DrawCanvas(pathData, pathList)
+          }
           ButtonPanel(
             { color ->
               pathData.value = pathData.value.copy(
@@ -46,8 +99,8 @@ class MainActivity : ComponentActivity() {
               )
             }
           ) {
-            pathList.removeIf{
-              pathD -> pathList[pathList.size - 1] == pathD
+            pathList.removeIf { pathD ->
+              pathList[pathList.size - 1] == pathD
             }
           }
         }
